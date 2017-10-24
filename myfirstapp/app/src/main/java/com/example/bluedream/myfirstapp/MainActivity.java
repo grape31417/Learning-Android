4031606d205e40f47ee391b7f00b8a7ed880b151
BlueDream <498g0104@stust.edu.tw>
2017-10-24 11:46:04 +0800

    add error log

--- myfirstapp/app/src/main/java/com/example/bluedream/myfirstapp/MainActivity.java
+++ myfirstapp/app/src/main/java/com/example/bluedream/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bluedream.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,14 +35,35 @@ public class MainActivity extends AppCompatActivity {
            String strSex=mEdtSex.getText().toString();
            int iAge=Integer.parseInt(mEdtAge.getText().toString());
            String  strSug =getString(R.string.suggestion);
-           if (strSex.equals(getString(R.string.sex_male)))
-               if(iAge<28)strSug+=getString(R.string.sug_not_hurry);
-               else if(iAge>33)strSug+=getString(R.string.sug_get_married);
-               else strSug+=getString(R.string.sug_find_couple);
-           else
-               if(iAge<28)strSug+=getString(R.string.sug_not_hurry);
-               else if(iAge>33)strSug+=getString(R.string.sug_get_married);
-               else strSug+=getString(R.string.sug_find_couple);
+           if (strSex.equals(getString(R.string.sex_male))) {
+               if (iAge < 28) {
+                   strSug += getString(R.string.sug_not_hurry);
+                   Log.d("Marrisug", "man,don't hurry");
+               } else if (iAge > 33) {
+                   strSug += getString(R.string.sug_get_married);
+                   Log.d("Marrisug", "man, hurry to get married!");
+               } else {
+                   strSug += getString(R.string.sug_find_couple);
+                   Log.d("Marrisug", "man,start to find girl friend");
+               }
+           }
+           else{
+               if (iAge < 28)
+               {
+                   strSug += getString(R.string.sug_not_hurry);
+                   Log.d("Marrisug", "woman,don't hurry");
+               }
+               else if (iAge > 33)
+               {
+                   strSug += getString(R.string.sug_get_married);
+                   Log.d("Marrisug", "woman, hurry to get married!");
+               }
+               else
+               {
+                   strSug += getString(R.string.sug_find_couple);
+                   Log.d("Marrisug", "woman,start to find boy friend");
+               }
+           }
 
            mTxtR.setText(strSug);
 
