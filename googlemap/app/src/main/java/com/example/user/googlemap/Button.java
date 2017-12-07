ce9612813dd75fc7e3e39390d29004bb74347e01
wally <1099105319@kuas.edu.tw>
2017-12-07 18:44:27 +0800

    GOOGLE map 定位

--- googlemap/app/src/main/java/com/example/user/googlemap/Button.java
+++ googlemap/app/src/main/java/com/example/user/googlemap/Button.java
@@ -5,32 +5,121 @@ import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.example.user.googlemap.MapsActivity.mLocation;
 import static com.example.user.googlemap.MapsActivity.mMap;
+import static com.example.user.googlemap.MapsActivity.mMarker1;
+import static com.example.user.googlemap.MapsActivity.mMarker2;
+import static com.example.user.googlemap.MapsActivity.mMarker3;
+import static com.example.user.googlemap.MapsActivity.mMarker4;
+import static com.example.user.googlemap.MapsActivity.mPolylineRoute;
 
 /**
  * Created by USER on 2017/12/6.
  */
 
 public class Button extends android.widget.Button implements View.OnClickListener {
-    public Button(Context context) {
+    String mode;
+    public Button(Context context,String mode) {
         super(context);
+        this.mode=mode;
     }
 
     @Override
     public void onClick(View v) {
-        if (v==MapsActivity.mbtn3dMap)
-        {
-            // 設定地圖的俯視角度，並且放大到一定的等級，讓3D建築物出現。
-            CameraUpdate camUpdate = CameraUpdateFactory.newCameraPosition(
-                    new CameraPosition.Builder()
-                            .target(mMap.getCameraPosition().target)
-                            .tilt(60)
-                            .zoom(18)
-                            .build());
-            mMap.animateCamera(camUpdate);
-        }
+      switch (mode) {
+          case "3dmap":
+          // 設定地圖的俯視角度，並且放大到一定的等級，讓3D建築物出現。
+          CameraUpdate camUpdate = CameraUpdateFactory.newCameraPosition(
+                  new CameraPosition.Builder()
+                          .target(mMap.getCameraPosition().target)
+                          .tilt(60)
+                          .zoom(18)
+                          .build());
+          mMap.animateCamera(camUpdate);
+          case "btnAddMarkerOnClick":
+              if (mMarker1 == null) {
+                  String[] sLocation = mLocation[0][1].split(",");
+                  double dLat = Double.parseDouble(sLocation[0]);
+                  double dLon = Double.parseDouble(sLocation[1]);
+                  mMarker1 = mMap.addMarker(new MarkerOptions()
+                          .title(mLocation[0][0])
+                          .snippet("2004年完工,高509公尺")
+                          .position(new LatLng(dLat, dLon))
+                          .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrows))
+                          .anchor(0.5f, 0.5f));
+              }
 
-    }
+              if (mMarker2 == null) {
+                  String[] sLocation = mLocation[1][1].split(",");
+                  double dLat = Double.parseDouble(sLocation[0]);
+                  double dLon = Double.parseDouble(sLocation[1]);
+                  mMarker2 = mMap.addMarker(new MarkerOptions()
+                          .title(mLocation[1][0])
+                          .snippet("東起山海關,西至嘉峪關,全長6000多公里")
+                          .position(new LatLng(dLat, dLon))
+                          .icon(BitmapDescriptorFactory.fromResource(R.drawable.circle))
+                          .anchor(0.5f, 0.5f));
+              }
+
+              if (mMarker3 == null) {
+                  String[] sLocation = mLocation[2][1].split(",");
+                  double dLat = Double.parseDouble(sLocation[0]);
+                  double dLon = Double.parseDouble(sLocation[1]);
+                  mMarker3 = mMap.addMarker(new MarkerOptions()
+                          .title(mLocation[2][0])
+                          .snippet("1886年完工,高93公尺")
+                          .position(new LatLng(dLat, dLon))
+                          .icon(BitmapDescriptorFactory.fromResource(R.drawable.square))
+                          .anchor(0.5f, 0.5f));
+              }
+
+              if (mMarker4 == null) {
+                  String[] sLocation = mLocation[3][1].split(",");
+                  double dLat = Double.parseDouble(sLocation[0]);
+                  double dLon = Double.parseDouble(sLocation[1]);
+                  mMarker4 = mMap.addMarker(new MarkerOptions()
+                          .title(mLocation[3][0])
+                          .snippet("又稱為艾菲爾鐵塔,高324公尺")
+                          .position(new LatLng(dLat, dLon))
+                          .icon(BitmapDescriptorFactory.fromResource(R.drawable.star))
+                          .anchor(0.5f, 0.5f));
+              }
+              break;
+          case "btnRemoveMarkerOnClick":
+              if (mMarker1 != null) {
+                  mMarker1.remove();
+                  mMarker1 = null;
+              }
+              if (mMarker2 != null) {
+                  mMarker2.remove();
+                  mMarker2 = null;
+              }
+              if (mMarker3 != null) {
+                  mMarker3.remove();
+                  mMarker3 = null;
+              }
+              if (mMarker4 != null) {
+                  mMarker4.remove();
+                  mMarker4 = null;
+              }
+              break;
+
+          case "btnShowRouteOnClick":
+              mPolylineRoute.setVisible(true);
+
+              break;
+
+          case "btnHideRouteOnClick":
+              mPolylineRoute.setVisible(false);
+              break;
+
+      }
+    };
+
+
 }
--- googlemap/app/src/main/java/com/example/user/googlemap/MapsActivity.java
+++ googlemap/app/src/main/java/com/example/user/googlemap/MapsActivity.java
@@ -1,27 +1,37 @@
 package com.example.user.googlemap;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationSource, android.location.LocationListener {
 
 
     protected static GoogleMap mMap;
@@ -36,15 +46,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private SupportMapFragment mSupportMapFragment;
     protected static boolean mbIsZoomFirst = true;
-    private Marker mMarker1, mMarker2, mMarker3, mMarker4;
-    private Polyline mPolylineRoute;
+    protected static Marker mMarker1, mMarker2, mMarker3, mMarker4;
+    protected static Polyline mPolylineRoute;
+    private final int REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION = 100;
+    private LocationManager mLocationMgr;
+    private OnLocationChangedListener mLocationChangedListener;
 
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maps);
         mSpnLocation = (Spinner) findViewById(R.id.spnLocation);
         ArrayAdapter<String> arrAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
         for (int i = 0; i < mLocation.length; i++) {
@@ -60,7 +73,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         spnMapType.setOnItemSelectedListener(new adapterOnITEMClick(spnMapType.getContext()));
 
         mbtn3dMap = (Button) findViewById(R.id.btn3DMap);
-        mbtn3dMap.setOnClickListener(new com.example.user.googlemap.Button(mbtn3dMap.getContext()));
+        mbtn3dMap.setOnClickListener(new com.example.user.googlemap.Button(mbtn3dMap.getContext(), "3dmap"));
 
         mSupportMapFragment = new SupportMapFragment();
         mSupportMapFragment.getMapAsync(this);
@@ -69,14 +82,110 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .add(R.id.frameLayMapContainer, mSupportMapFragment)
                 .commit();
 
+        mLocationMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         Button btnAddMarker = (Button) findViewById(R.id.btnAddMarker);
-        btnAddMarker.setOnClickListener(btnAddMarkerOnClick);
+        btnAddMarker.setOnClickListener(new com.example.user.googlemap.Button(mbtn3dMap.getContext(), "btnAddMarkerOnClick"));
         Button btnRemoveMarker = (Button) findViewById(R.id.btnRemoveMarker);
-        btnRemoveMarker.setOnClickListener(btnRemoveMarkerOnClick);
+        btnRemoveMarker.setOnClickListener(new com.example.user.googlemap.Button(mbtn3dMap.getContext(), "btnRemoveMarkerOnClick"));
         Button btnShowRoute = (Button) findViewById(R.id.btnShowRoute);
-        btnShowRoute.setOnClickListener(btnShowRouteOnClick);
+        btnShowRoute.setOnClickListener(new com.example.user.googlemap.Button(mbtn3dMap.getContext(), "btnShowRouteOnClick"));
         Button btnHideRoute = (Button) findViewById(R.id.btnHideRoute);
-        btnHideRoute.setOnClickListener(btnHideRouteOnClick);
+        btnHideRoute.setOnClickListener(new com.example.user.googlemap.Button(mbtn3dMap.getContext(), "btnHideRouteOnClick"));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // App從背景切換到前景執行，啟動定位功能。
+        if (mMap != null)
+            checkLocationPermissionAndEnableLocationUpdate(true);
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // 停止定位功能。
+        checkLocationPermissionAndEnableLocationUpdate(false);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 檢查收到的權限要求編號是否和我們送出的相同
+        if (requestCode == REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 再檢查一次，就會進入同意的狀態，並且順利啟動。
+                checkLocationPermissionAndEnableLocationUpdate(true);
+                return;
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void checkLocationPermissionAndEnableLocationUpdate(boolean on) {
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // 這項功能尚未取得使用者的同意
+            // 開始執行徵詢使用者的流程
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    MapsActivity.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder altDlgBuilder = new AlertDialog.Builder(MapsActivity.this);
+                altDlgBuilder.setTitle("提示");
+                altDlgBuilder.setMessage("App需要啟動定位功能。");
+                altDlgBuilder.setIcon(android.R.drawable.ic_dialog_info);
+                altDlgBuilder.setCancelable(false);
+                altDlgBuilder.setPositiveButton("確定",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // 顯示詢問使用者是否同意功能權限的對話盒
+                                // 使用者答覆後會執行callback方法onRequestPermissionsResult()
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{
+                                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION);
+                            }
+                        });
+                altDlgBuilder.show();
+
+                return;
+            } else {
+                // 顯示詢問使用者是否同意功能權限的對話盒
+                // 使用者答覆後會執行callback方法onRequestPermissionsResult()
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION);
+
+                return;
+            }
+        }
+
+        // 這項功能之前已經取得使用者的同意
+        // 根據on參數的值，啟動或關閉定位功能
+        if (on) {
+            // 如果GPS功能有開啟，優先使用GPS定位，否則使用網路定位。
+            if (mLocationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                mLocationMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, (android.location.LocationListener) this);
+                Toast.makeText(MapsActivity.this, "使用GPS定位", Toast.LENGTH_LONG)
+                        .show();
+            } else if (mLocationMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                mLocationMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, (android.location.LocationListener) this);
+                Toast.makeText(MapsActivity.this, "使用網路定位", Toast.LENGTH_LONG)
+                        .show();
+            }
+        } else {
+            mLocationMgr.removeUpdates((android.location.LocationListener) this);
+            Toast.makeText(MapsActivity.this, "定位功能已經停用", Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
 
@@ -92,6 +201,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        mMap.setMyLocationEnabled(true);   // 如果在模擬器執行，要加上這行程式碼
+        mMap.setLocationSource(this);
+
+        // 取得上一次定位資料。
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            Location location =
+                    mLocationMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location == null)
+                location =
+                        mLocationMgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if (location != null) {
+                Toast.makeText(MapsActivity.this, "成功取得上一次定位",
+                        Toast.LENGTH_LONG).show();
+                onLocationChanged(location);    // 更新地圖的定位。
+            } else
+                Toast.makeText(MapsActivity.this, "沒有上一次定位的資料",
+                        Toast.LENGTH_LONG).show();
+        }
 
         // 設定Google Map的Info Window。
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
@@ -134,100 +265,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         polylineOpt.addAll(listLatLng);
         mPolylineRoute = mMap.addPolyline(polylineOpt);
         mPolylineRoute.setVisible(false);
+
+
+
     }
 
 
-    private View.OnClickListener btnAddMarkerOnClick = new View.OnClickListener() {
+    @Override
+    public void onLocationChanged(Location location) {
+        // 把新位置傳給Google Map的my-location layer。
+        if (mLocationChangedListener != null)
+            mLocationChangedListener.onLocationChanged(location);
 
-        @Override
-        public void onClick(View v) {
-            // 在4個景點加上Marker。
-            if (mMarker1 == null) {
-                String[] sLocation = mLocation[0][1].split(",");
-                double dLat = Double.parseDouble(sLocation[0]);
-                double dLon = Double.parseDouble(sLocation[1]);
-                mMarker1 = mMap.addMarker(new MarkerOptions()
-                        .title(mLocation[0][0])
-                        .snippet("2004年完工,高509公尺")
-                        .position(new LatLng(dLat, dLon))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrows))
-                        .anchor(0.5f, 0.5f));
-            }
+        // 移動地圖到新位置。
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(
+                new LatLng(location.getLatitude(), location.getLongitude())));
 
-            if (mMarker2 == null) {
-                String[] sLocation = mLocation[1][1].split(",");
-                double dLat = Double.parseDouble(sLocation[0]);
-                double dLon = Double.parseDouble(sLocation[1]);
-                mMarker2 = mMap.addMarker(new MarkerOptions()
-                        .title(mLocation[1][0])
-                        .snippet("東起山海關,西至嘉峪關,全長6000多公里")
-                        .position(new LatLng(dLat, dLon))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.circle))
-                        .anchor(0.5f, 0.5f));
-            }
+    }
 
-            if (mMarker3 == null) {
-                String[] sLocation = mLocation[2][1].split(",");
-                double dLat = Double.parseDouble(sLocation[0]);
-                double dLon = Double.parseDouble(sLocation[1]);
-                mMarker3 = mMap.addMarker(new MarkerOptions()
-                        .title(mLocation[2][0])
-                        .snippet("1886年完工,高93公尺")
-                        .position(new LatLng(dLat, dLon))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.square))
-                        .anchor(0.5f, 0.5f));
-            }
-
-            if (mMarker4 == null) {
-                String[] sLocation = mLocation[3][1].split(",");
-                double dLat = Double.parseDouble(sLocation[0]);
-                double dLon = Double.parseDouble(sLocation[1]);
-                mMarker4 = mMap.addMarker(new MarkerOptions()
-                        .title(mLocation[3][0])
-                        .snippet("又稱為艾菲爾鐵塔,高324公尺")
-                        .position(new LatLng(dLat, dLon))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.star))
-                        .anchor(0.5f, 0.5f));
-            }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        String str = provider;
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                str += "定位功能無法使用";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                str += "暫時無法定位";	// GPS正在定位中時會傳入這個值。
+                break;
         }
-    };
 
-    private View.OnClickListener btnRemoveMarkerOnClick = new View.OnClickListener() {
+        Toast.makeText(MapsActivity.this, str, Toast.LENGTH_LONG)
+                .show();
+    }
 
-        @Override
-        public void onClick(View v) {
-            if (mMarker1 != null) {
-                mMarker1.remove();
-                mMarker1 = null;
-            }
-            if (mMarker2 != null) {
-                mMarker2.remove();
-                mMarker2 = null;
-            }
-            if (mMarker3 != null) {
-                mMarker3.remove();
-                mMarker3 = null;
-            }
-            if (mMarker4 != null) {
-                mMarker4.remove();
-                mMarker4 = null;
-            }
-        }
-    };
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(MapsActivity.this, provider + "定位功能開啟",
+                Toast.LENGTH_LONG).show();
+        checkLocationPermissionAndEnableLocationUpdate(true);
+    }
 
-    private View.OnClickListener btnShowRouteOnClick = new View.OnClickListener() {
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(MapsActivity.this, provider + "定位功能已經被關閉",
+                Toast.LENGTH_LONG).show();
+        checkLocationPermissionAndEnableLocationUpdate(false);
+    }
 
-        @Override
-        public void onClick(View v) {
-            mPolylineRoute.setVisible(true);
-        }
-    };
 
-    private View.OnClickListener btnHideRouteOnClick = new View.OnClickListener() {
 
-        @Override
-        public void onClick(View v) {
-            mPolylineRoute.setVisible(false);
-        }
-    };
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mLocationChangedListener = onLocationChangedListener;
+        checkLocationPermissionAndEnableLocationUpdate(true);
+        Toast.makeText(MapsActivity.this, "地圖的my-location layer已經啟用",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void deactivate() {
+        mLocationChangedListener = null;
+        checkLocationPermissionAndEnableLocationUpdate(false);
+        Toast.makeText(MapsActivity.this, "地圖的my-location layer已經關閉",
+                Toast.LENGTH_LONG).show();
+    }
+
+
 }
